7bc0bd1a0861d51296f96a4072b35be8caba1ae0
Sagar Khanpara <sagar.k@simformsolutions.com>
2021-03-25 18:27:11 +0530

    Update home page with images and heading

--- src/App.js
+++ src/App.js
@@ -13,9 +13,61 @@ export default class App extends Lightning.Component {
         rect: true,
         color: 0xff000000,
       },
+
+      Header: {
+        rect: true,
+        w: 1920,
+        h: 90,
+        color: 0xff000000,
+        MyImage: {
+          x: 40,
+          y: 30,
+          h: 51,
+          w: 176,
+          src: "../static/images/netflix.png",
+        },
+        Title: {
+          x: (w) => w - 95,
+          mountX: 1,
+          mountY: 0.5,
+          y: 40,
+          // text: { text: "Header" },
+          MyImage: {
+            x: 0,
+            y: 0,
+            h: 26,
+            w: 25,
+            src: "../static/images/GridView.png",
+          },
+        },
+        Title2: {
+          x: (w) => w - 70,
+          mountX: 1,
+          mountY: 0.5,
+          y: 40,
+          // text: { text: "Header" },
+          MyImage: {
+            x: 0,
+            y: 0,
+            h: 26,
+            w: 25,
+            src: "../static/images/ListView.png",
+          },
+        },
+      },
+      MyFlexBoxTitle: {
+        x: 30,
+        y: 140,
+        text: {
+          text: "Featured List",
+          fontSize: 26,
+          fontStyle: "bold",
+        },
+      },
+
       MyFlexBox: {
         x: 20,
-        y: 20,
+        y: 170,
         w: 1920,
         flex: { direction: "row", padding: 0, wrap: false },
         rect: false,
@@ -25,6 +77,56 @@ export default class App extends Lightning.Component {
           flexItem: { margin: 10 },
           rect: true,
           color: 0xff444444,
+          UsingTexture: {
+            x: 0,
+            y: 0,
+            w: 450,
+            h: 250,
+            texture: {
+              type: lng.textures.ImageTexture,
+              src: "../static/images/image5.png",
+            },
+          },
+          MyFlexItemChildOne: {
+            x: 0,
+            y: 0,
+            rect: true,
+            w: 450,
+            h: 250,
+            color: "none",
+
+            MyFlexOverlyBlack: {
+              x: 0,
+              y: 0,
+              rect: true,
+              w: 450,
+              h: 250,
+              color: 0xff000000,
+              alpha: 0.6,
+            },
+
+            MyFlexBoxTitle: {
+              x: 30,
+              y: 140,
+              text: {
+                text: "Monster Hunter",
+                fontSize: 26,
+                fontStyle: "bold",
+              },
+            },
+
+            MyFlexBoxDesc: {
+              x: 30,
+              y: 180,
+              w: 400,
+              text: {
+                text:
+                  "In a near-future world where an oppressive regime segregates...",
+                fontSize: 18,
+                lineHeight: 24,
+              },
+            },
+          },
         },
         MyFlexItemTwo: {
           w: 450,
@@ -32,6 +134,16 @@ export default class App extends Lightning.Component {
           flexItem: { margin: 10 },
           rect: true,
           color: 0xff999999,
+          UsingTexture: {
+            x: 0,
+            y: 0,
+            w: 450,
+            h: 250,
+            texture: {
+              type: lng.textures.ImageTexture,
+              src: "../static/images/image6.png",
+            },
+          },
         },
         MyFlexItemThree: {
           w: 450,
@@ -39,6 +151,16 @@ export default class App extends Lightning.Component {
           flexItem: { margin: 10 },
           rect: true,
           color: 0xff999999,
+          UsingTexture: {
+            x: 0,
+            y: 0,
+            w: 450,
+            h: 250,
+            texture: {
+              type: lng.textures.ImageTexture,
+              src: "../static/images/image7.png",
+            },
+          },
         },
         MyFlexItemFour: {
           w: 450,
@@ -46,6 +168,16 @@ export default class App extends Lightning.Component {
           flexItem: { margin: 10 },
           rect: true,
           color: 0xff999999,
+          UsingTexture: {
+            x: 0,
+            y: 0,
+            w: 450,
+            h: 250,
+            texture: {
+              type: lng.textures.ImageTexture,
+              src: "../static/images/image1.png",
+            },
+          },
         },
         // MyFlexBoxItemWithFlexItemChildren: {
         //   flex: { direction: "row", padding: 20 },
@@ -60,9 +192,19 @@ export default class App extends Lightning.Component {
         // },
       },
 
+      MyFlexBoxTitleTwo: {
+        x: 30,
+        y: 470,
+        text: {
+          text: "Recently Added",
+          fontSize: 26,
+          fontStyle: "bold",
+        },
+      },
+
       MyFlexBoxTwo: {
         x: 20,
-        y: 320,
+        y: 500,
         w: 1920,
         flex: { direction: "row", padding: 0, wrap: false },
         rect: false,
@@ -78,7 +220,7 @@ export default class App extends Lightning.Component {
             h: 250,
             texture: {
               type: lng.textures.ImageTexture,
-              src: "../static/images/logo.png",
+              src: "../static/images/image1.png",
             },
           },
           flexItem: { margin: 10 },
@@ -91,6 +233,16 @@ export default class App extends Lightning.Component {
           flexItem: { margin: 10 },
           rect: true,
           color: 0xff999999,
+          UsingTexture: {
+            x: 0,
+            y: 0,
+            w: 450,
+            h: 250,
+            texture: {
+              type: lng.textures.ImageTexture,
+              src: "../static/images/image2.png",
+            },
+          },
         },
         MyFlexItemThree: {
           w: 450,
@@ -98,6 +250,16 @@ export default class App extends Lightning.Component {
           flexItem: { margin: 10 },
           rect: true,
           color: 0xff999999,
+          UsingTexture: {
+            x: 0,
+            y: 0,
+            w: 450,
+            h: 250,
+            texture: {
+              type: lng.textures.ImageTexture,
+              src: "../static/images/image3.png",
+            },
+          },
         },
         MyFlexItemFour: {
           w: 450,
@@ -105,6 +267,16 @@ export default class App extends Lightning.Component {
           flexItem: { margin: 10 },
           rect: true,
           color: 0xff999999,
+          UsingTexture: {
+            x: 0,
+            y: 0,
+            w: 450,
+            h: 250,
+            texture: {
+              type: lng.textures.ImageTexture,
+              src: "../static/images/image4.png",
+            },
+          },
         },
         // MyFlexBoxItemWithFlexItemChildren: {
         //   flex: { direction: "row", padding: 20 },
